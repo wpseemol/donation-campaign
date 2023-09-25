36db1f032a9f89fef04d1b79e0303bf2e22fbc96
wpseemol <seemol.contact@gmail.com>
2023-09-26 00:29:49 +0600

    reactChart install and show on my project

--- donation-campaign/src/pages/Statistics/Statistics.jsx
+++ donation-campaign/src/pages/Statistics/Statistics.jsx
@@ -1,7 +1,24 @@
+import { Cell, Pie, PieChart } from "recharts";
+const data = [
+  {
+    name: "Group A",
+    value: 400,
+  },
+  {
+    name: "Group B",
+    value: 300,
+  },
+];
 const Statistics = () => {
   return (
-    <div className="text-center text-5xl font-black mt-28 capitalize">
-      this is Statistics Componet
+    <div className="flex justify-center items-center">
+      <PieChart width={730} height={250}>
+        <Pie data={data} cx="50%" cy="50%" outerRadius={80} label>
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} />
+          ))}
+        </Pie>
+      </PieChart>
     </div>
   );
 };
