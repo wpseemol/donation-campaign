e82ff82d3e5f50e13d90d40ada319c276410dd86
wpseemol <seemol.contact@gmail.com>
2023-09-27 18:56:55 +0600

    donation section No data Found set

--- donation-campaign/src/pages/Donation/Donation.jsx
+++ donation-campaign/src/pages/Donation/Donation.jsx
@@ -21,32 +21,42 @@ const Donation = () => {
     }
   }, []);
 
-  return (
-    <>
-      <div className="grid sm:grid-cols-2 grid-cols-1 gap-6 mx-3 mt-48 sm:mt-0">
-        {dntItems &&
-          dntItems
-            .slice(0, dataLlengtth)
-            .map((cotent) => (
-              <CardComponent key={cotent.id} content={cotent} />
-            ))}
-      </div>
-      <div
-        className={
-          dntItems.length < 4 || dataLlengtth === dntItems.length
-            ? "hidden"
-            : "flex justify-center items-center my-14"
-        }
-      >
-        <button
-          onClick={() => setDatalenth(dntItems.length)}
-          className="btn bg-green-800 text-white"
+  if (dntItems.length <= 0) {
+    return (
+      <main className="myContainer">
+        <div className="grid sm:grid-cols-2 grid-cols-1 gap-6 mx-3 mt-48 sm:mt-0">
+          {dntItems &&
+            dntItems
+              .slice(0, dataLlengtth)
+              .map((cotent) => (
+                <CardComponent key={cotent.id} content={cotent} />
+              ))}
+        </div>
+        <div
+          className={
+            dntItems.length < 4 || dataLlengtth === dntItems.length
+              ? "hidden"
+              : "flex justify-center items-center my-14"
+          }
         >
-          See All
-        </button>
-      </div>
-    </>
-  );
+          <button
+            onClick={() => setDatalenth(dntItems.length)}
+            className="btn bg-green-800 text-white"
+          >
+            See All
+          </button>
+        </div>
+      </main>
+    );
+  } else {
+    return (
+      <main className="myContainer mt-96 sm:mt-80 ">
+        <div className="flex justify-center ">
+          <h2 className="text-4xl font-bold"> Data Not Found </h2>
+        </div>
+      </main>
+    );
+  }
 };
 
 export default Donation;
